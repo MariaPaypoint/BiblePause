404ea4ff41f15838d1358db492381feb7e8bc378
Maria <maria@paypoint.pro>
2024-05-09 14:25:14 +0300

    Initial Commit

---
+++ BiblePause/BiblePauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiblePauseApp.swift
+//  BiblePause
+//
+//  Created by Maria Novikova on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BiblePauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiblePause/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiblePause
+//
+//  Created by Maria Novikova on 09.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
